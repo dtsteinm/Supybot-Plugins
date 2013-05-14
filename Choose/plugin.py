b741489a0b0c14292591e3e2100f415206e7f884
dtsteinm <dtsteinm@gmail.com>
2013-05-14 11:48:42 -0400

    Added checking against Infobot personality.

--- Choose/plugin.py
+++ Choose/plugin.py
@@ -7,21 +7,26 @@
 #
 ###
 
+# TODO: Conform to supybot standards; document code.
+
 from random import choice
 
-import supybot.utils as utils
+# import supybot.utils as utils
 from supybot.commands import *
-import supybot.plugins as plugins
-import supybot.ircutils as ircutils
+# import supybot.plugins as plugins
+# import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
+import supybot.conf as conf
 
+no_choice_reply = 'Please provide two or more options.'
 zero_choice_reply = ['You gotta give me something...', ]
 one_choice_reply = ['So, it\'s between that and...', ]
+
 
 class Choose(callbacks.Plugin):
     """Make tough decisions between two or more options, easily."""
     def __init__(self, irc):
-          callbacks.Plugin.__init__(self, irc)
+        callbacks.Plugin.__init__(self, irc)
 
     def choose(self, irc, msg, args, choices):
         """<choice1> ... <choiceN>
@@ -29,14 +34,20 @@ class Choose(callbacks.Plugin):
         Randomly selects one of multiple choices.
         """
         if choices is None:
-            irc.reply(choice(zero_choice_reply))
+            if conf.get(conf.supybot.plugins.Infobot.personality):
+                irc.reply(choice(zero_choice_reply))
+            else:
+                irc.reply(no_choice_reply)
             return
 
         options = []
         options += [y.strip() for x in choices.split(' or ')
-                for y in x.split(';') if y not in ['', ' ']]
+                    for y in x.split(';') if y not in ['', ' ']]
         if len(options) is 1:
-            irc.reply(choice(one_choice_reply))
+            if conf.get(conf.supybot.plugins.Infobot.personality):
+                irc.reply(choice(one_choice_reply))
+            else:
+                irc.reply(no_choice_reply)
             return
         else:
             irc.reply(choice(options))
